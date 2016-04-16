9911b1fc0a488d7deb9c7a4a03676f4f0e76cdc6
keli <keli@keli.keli>
2016-04-16 16:48:09 +0200

    admin: added car and passenger to admin

--- travel/admin.py
+++ travel/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from travel.models import Passenger, Car
+
+
+class PassengerInline(admin.TabularInline):
+    model = Passenger
+    fk_name = 'car'
+    extra = 5
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'driver', 'car_name', 'price', 'get_num_of_free_seats', 'start_time', 'start_location')
+    list_filter = ['num_of_seats']
+    search_fields = ['driver']
+    inlines = [PassengerInline]
+
+admin.site.register(Car, CarAdmin)
