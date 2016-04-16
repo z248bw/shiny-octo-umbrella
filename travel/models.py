3a23bd2208da7d1ba76962678eb64d275f65cddc
keli <keli@keli.keli>
2016-04-16 16:29:01 +0200

    models: added car and passenger models

--- travel/models.py
+++ travel/models.py
@@ -1,3 +1,34 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    driver = models.ForeignKey(User, related_name='driver', verbose_name='Sofor')
+    car_name = models.CharField(max_length=20, verbose_name='Auto tipusa')
+    price = models.IntegerField(verbose_name='Ar')
+    num_of_seats = models.IntegerField(verbose_name='Ferohelyek szama(soforules nelkul)')
+    start_time = models.DateTimeField(verbose_name='Indulasi ido')
+    start_location = models.CharField(max_length=100, verbose_name='Indulasi hely')
+    description = models.CharField(max_length=100, verbose_name='Egyeb', null=True, blank=True)
+
+    class NoMoreSpaceException(Exception):
+        pass
+
+    def get_passengers(self):
+        return Passenger.objects.filter(car=self.pk)
+
+    def get_num_of_free_seats(self):
+        return self.num_of_seats - len(self.get_passengers())
+
+
+class Passenger(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    car = models.ForeignKey(Car, related_name='car', verbose_name='Auto')
+
+    @staticmethod
+    def take_a_seat(user, car):
+        passenger = Passenger(user=user, car=car)
+        if car.get_num_of_free_seats() == 0:
+            raise Car.NoMoreSpaceException
+        passenger.save()
+        return user
--- travel/tests.py
+++ travel/tests.py
@@ -1,3 +1,90 @@
-from django.test import TestCase
+from datetime import datetime
 
-# Create your tests here.
+from django.contrib.auth.models import User
+from django.test import TestCase
+import random, string
+
+from travel.models import Car, Passenger
+
+
+class SimpleTest(TestCase):
+    def test_basic_addition(self):
+        """
+        Tests that 1 + 1 always equals 2.
+        """
+        self.assertEqual(1 + 1, 2)
+
+
+class CarTest(TestCase):
+    # http: // stackoverflow.com / questions / 2030053 / random - strings - in -python
+    def randomword(self, length):
+        return ''.join(random.choice(string.ascii_lowercase) for i in range(length))
+
+    def create_passenger_user(self, car):
+        user = User(username=self.randomword(5),
+                    password='')
+        user.save()
+        return Passenger.take_a_seat(user=user, car=car)
+
+    def setUp(self):
+        self.driver = User(username='user', password='')
+        self.driver.save()
+        self.car1 = Car(driver=self.driver,
+                        car_name='ford',
+                        price=0,
+                        num_of_seats=4,
+                        start_time=datetime(2015, 1, 1, 12, 0, 0),
+                        start_location='asd street')
+        self.car1.save()
+        self.car2 = Car(driver=self.driver,
+                        car_name='ford',
+                        price=0,
+                        num_of_seats=4,
+                        start_time=datetime(2015, 1, 1, 12, 0, 0),
+                        start_location='asd street')
+        self.car2.save()
+
+    def test_create_car_passenger_if_car_has_no_free_seat(self):
+        car_with_no_space = Car(driver=self.driver,
+                                car_name='ford',
+                                price=0,
+                                num_of_seats=0,
+                                start_time=datetime(2015, 1, 1, 12, 0, 0),
+                                start_location='asd street')
+        car_with_no_space.save()
+        with self.assertRaisesMessage(expected_exception=Car.NoMoreSpaceException, expected_message=''):
+            self.create_passenger_user(car_with_no_space)
+
+    def test_create_car_passenger_if_car_has_free_seats(self):
+        user = self.create_passenger_user(self.car1)
+        self.assertEqual(user.passenger.car, self.car1)
+
+    def assert_passengers(self, expected, actual):
+        for i, e in enumerate(expected):
+            self.assertEqual(e.pk, actual[i].pk)
+
+    def test_get_passengers_of_car_with_passengers_no_passengers(self):
+        self.assert_passengers(expected=[], actual=self.car1.get_passengers())
+
+    def test_get_passengers_of_car_with_one_passenger(self):
+        expected = [self.create_passenger_user(self.car1).passenger]
+        self.assert_passengers(expected=expected, actual=self.car1.get_passengers())
+
+    def test_get_passengers_of_car_with_two_passengers(self):
+        expected = [self.create_passenger_user(self.car1).passenger,
+                    self.create_passenger_user(self.car1).passenger]
+        self.assert_passengers(expected=expected, actual=self.car1.get_passengers())
+
+    def test_get_passengers_of_car_with_passengers_in_the_other_car(self):
+        self.assert_passengers(expected=[], actual=self.car2.get_passengers())
+
+    def test_get_passengers_of_car_if_passengers_are_deleted(self):
+        user = self.create_passenger_user(self.car1)
+        self.assert_passengers(expected=[user.passenger], actual=self.car1.get_passengers())
+        user.delete()
+        self.assert_passengers(expected=[], actual=self.car1.get_passengers())
+
+    def test_get_num_of_free_seats(self):
+        self.create_passenger_user(self.car1)
+        self.create_passenger_user(self.car1)
+        self.assertEqual(2, self.car1.num_of_seats - 2)
