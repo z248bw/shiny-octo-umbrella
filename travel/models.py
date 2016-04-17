ace9f48f1fc852e49114f91ef43acf925424e466
keli <keli@keli.keli>
2016-04-17 23:13:28 +0200

    model: refactored models to add phone attribute to driver

--- travel/models.py
+++ travel/models.py
@@ -2,13 +2,24 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
+class AbstractPerson(models.Model):
+    phone = models.CharField(max_length=15, verbose_name='Telefon', null=True, blank=True)
+
+    class Meta:
+        abstract = True
+
+
+class Driver(AbstractPerson):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
 class Car(models.Model):
-    driver = models.ForeignKey(User, related_name='driver', verbose_name='Sofor')
-    car_name = models.CharField(max_length=20, verbose_name='Auto tipusa')
+    driver = models.OneToOneField(Driver, on_delete=models.CASCADE)
     price = models.IntegerField(verbose_name='Ar')
     num_of_seats = models.IntegerField(verbose_name='Ferohelyek szama(soforules nelkul)')
     start_time = models.DateTimeField(verbose_name='Indulasi ido')
     start_location = models.CharField(max_length=100, verbose_name='Indulasi hely')
+    car_name = models.CharField(max_length=20, verbose_name='Auto tipusa', null=True, blank=True)
     description = models.CharField(max_length=100, verbose_name='Egyeb', null=True, blank=True)
 
     class NoDriverContactProvidedException(Exception):
@@ -21,17 +32,17 @@ class Car(models.Model):
         return self.num_of_seats - len(self.get_passengers())
 
     def save(self, *args, **kwargs):
-        self.check_driver()
+        self.check_driver_contact_info()
         super(Car, self).save(*args, **kwargs)
 
-    def check_driver(self):
-        if self.driver.last_name == '' \
-                or self.driver.first_name == '' \
-                or self.driver.email == '':
+    def check_driver_contact_info(self):
+        if self.driver.user.last_name == '' \
+                or self.driver.user.first_name == '' \
+                or self.driver.user.email == '':
             raise Car.NoDriverContactProvidedException
 
 
-class Passenger(models.Model):
+class Passenger(AbstractPerson):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     car = models.ForeignKey(Car, related_name='car', verbose_name='Auto')
 
@@ -43,7 +54,7 @@ class Passenger(models.Model):
 
     @staticmethod
     def take_a_seat(user, car):
-        if len(Car.objects.filter(driver=user)) > 0:
+        if len(Car.objects.filter(driver__user=user)) > 0:
             raise Passenger.DriverCannotBePassengerException
         passenger = Passenger(user=user, car=car)
         if car.get_num_of_free_seats() == 0:
--- travel/tests.py
+++ travel/tests.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 
 from django.utils.crypto import get_random_string
 
-from travel.models import Car, Passenger
+from travel.models import Car, Passenger, Driver
 
 
 def create_user():
@@ -35,39 +35,34 @@ class ModelTestBase:
 
 
 class CarTest(TestCase, ModelTestBase):
-    def setUp(self):
-        self.driver = create_user()
-        self.driver.save()
-        self.car1 = Car(driver=self.driver,
-                        car_name='ford',
-                        price=0,
-                        num_of_seats=4,
-                        start_time=datetime(2015, 1, 1, 12, 0, 0),
-                        start_location='asd street')
-        self.car1.save()
-        self.car2 = Car(driver=self.driver,
-                        car_name='ford',
-                        price=0,
-                        num_of_seats=4,
-                        start_time=datetime(2015, 1, 1, 12, 0, 0),
-                        start_location='asd street')
-        self.car2.save()
+    def create_car(self):
+        driver = Driver(user=create_user())
+        driver.save()
+        car = Car(driver=driver,
+                  price=0,
+                  num_of_seats=4,
+                  start_time=datetime(2015, 1, 1, 12, 0, 0),
+                  start_location='asd street')
+        car.save()
+        return car
 
     def test_save_car_without_driver_email(self):
         with self.assertRaisesMessage(expected_exception=Car.NoDriverContactProvidedException,
                                       expected_message=''):
             noname_user = User(username='noname', password='')
             noname_user.save()
-            Car(driver=noname_user,
-                car_name='ford',
+            noname_driver = Driver(user=noname_user)
+            noname_driver.save()
+            Car(driver=noname_driver,
                 price=0,
                 num_of_seats=0,
                 start_time=datetime(2015, 1, 1, 12, 0, 0),
                 start_location='asd street').save()
 
     def test_create_car_passenger_if_car_has_no_free_seat(self):
-        car_with_no_space = Car(driver=self.driver,
-                                car_name='ford',
+        driver = Driver(user=create_user())
+        driver.save()
+        car_with_no_space = Car(driver=driver,
                                 price=0,
                                 num_of_seats=0,
                                 start_time=datetime(2015, 1, 1, 12, 0, 0),
@@ -78,75 +73,94 @@ class CarTest(TestCase, ModelTestBase):
             self.create_passenger_user(car_with_no_space)
 
     def test_create_car_passenger_if_car_has_free_seats(self):
-        user = self.create_passenger_user(self.car1)
-        self.assertEqual(user.passenger.car, self.car1)
+        car = self.create_car()
+        user = self.create_passenger_user(car)
+        self.assertEqual(user.passenger.car, car)
 
     def test_add_same_passenger_to_car_multiple_times(self):
         user = User(username=get_random_string(length=5),
                     password='')
         user.save()
-        Passenger.take_a_seat(user=user, car=self.car1)
+        car = self.create_car()
+        Passenger.take_a_seat(user=user, car=car)
         with self.assertRaises(expected_exception=IntegrityError):
-            Passenger.take_a_seat(user=user, car=self.car1)
+            Passenger.take_a_seat(user=user, car=car)
 
     def test_add_driver_as_passenger(self):
-        car = self.create_car_with_driver()
+        car = self.create_car()
         with self.assertRaises(expected_exception=Passenger.DriverCannotBePassengerException,
                                expected_message=''):
-            Passenger.take_a_seat(user=car.driver, car=car)
+            Passenger.take_a_seat(user=car.driver.user, car=car)
 
     def test_add_driver_as_passenger_in_another_car(self):
-        car = self.create_car_with_driver()
+        car = self.create_car()
+        other_car = self.create_car()
         with self.assertRaises(expected_exception=Passenger.DriverCannotBePassengerException,
                                expected_message=''):
-            Passenger.take_a_seat(user=car.driver, car=self.car1)
+            Passenger.take_a_seat(user=car.driver.user, car=other_car)
 
-    def create_car_with_driver(self):
-        driver = create_user()
+    def test_driver_drives_multiple_cars(self):
+        driver = Driver(user=create_user())
         driver.save()
         car = Car(driver=driver,
-                  car_name='ford',
                   price=0,
-                  num_of_seats=4,
+                  num_of_seats=0,
                   start_time=datetime(2015, 1, 1, 12, 0, 0),
                   start_location='asd street')
         car.save()
-        return car
+        with self.assertRaises(expected_exception=IntegrityError,
+                               expected_message=''):
+            car = Car(driver=driver,
+                      price=0,
+                      num_of_seats=0,
+                      start_time=datetime(2015, 1, 1, 12, 0, 0),
+                      start_location='asd street')
+            car.save()
 
     def test_add_same_passenger_to_multiple_cars(self):
         user = User(username=get_random_string(length=5),
                     password='')
         user.save()
-        Passenger.take_a_seat(user=user, car=self.car1)
+        car = self.create_car()
+        other_car = self.create_car()
+        Passenger.take_a_seat(user=user, car=car)
         with self.assertRaises(expected_exception=IntegrityError):
-            Passenger.take_a_seat(user=user, car=self.car2)
+            Passenger.take_a_seat(user=user, car=other_car)
 
     def assert_passengers(self, expected, actual):
         for i, e in enumerate(expected):
             self.assertEqual(e.pk, actual[i].pk)
 
     def test_get_passengers_of_car_with_no_passengers(self):
-        self.assert_passengers(expected=[], actual=self.car1.get_passengers())
+        car = self.create_car()
+        self.assert_passengers(expected=[], actual=car.get_passengers())
 
     def test_get_passengers_of_car_with_one_passenger(self):
-        expected = [self.create_passenger_user(self.car1).passenger]
-        self.assert_passengers(expected=expected, actual=self.car1.get_passengers())
+        car = self.create_car()
+        expected = [self.create_passenger_user(car).passenger]
+        self.assert_passengers(expected=expected, actual=car.get_passengers())
 
     def test_get_passengers_of_car_with_two_passengers(self):
-        expected = [self.create_passenger_user(self.car1).passenger,
-                    self.create_passenger_user(self.car1).passenger]
-        self.assert_passengers(expected=expected, actual=self.car1.get_passengers())
+        car = self.create_car()
+        expected = [self.create_passenger_user(car).passenger,
+                    self.create_passenger_user(car).passenger]
+        self.assert_passengers(expected=expected, actual=car.get_passengers())
 
     def test_get_passengers_of_car_with_passengers_in_the_other_car(self):
-        self.assert_passengers(expected=[], actual=self.car2.get_passengers())
+        car = self.create_car()
+        other_car = self.create_car()
+        self.create_passenger_user(other_car)
+        self.assert_passengers(expected=[], actual=car.get_passengers())
 
     def test_get_passengers_of_car_if_passengers_are_deleted(self):
-        user = self.create_passenger_user(self.car1)
-        self.assert_passengers(expected=[user.passenger], actual=self.car1.get_passengers())
+        car = self.create_car()
+        user = self.create_passenger_user(car)
+        self.assert_passengers(expected=[user.passenger], actual=car.get_passengers())
         user.delete()
-        self.assert_passengers(expected=[], actual=self.car1.get_passengers())
+        self.assert_passengers(expected=[], actual=car.get_passengers())
 
     def test_get_num_of_free_seats(self):
-        self.create_passenger_user(self.car1)
-        self.create_passenger_user(self.car1)
-        self.assertEqual(2, self.car1.num_of_seats - 2)
+        car = self.create_car()
+        self.create_passenger_user(car)
+        self.create_passenger_user(car)
+        self.assertEqual(2, car.num_of_seats - 2)
