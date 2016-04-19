abacfc068971c1ecb147289ef538bac440a8f125
keli <keli@keli.keli>
2016-04-19 22:43:00 +0200

    rest: added user object level authorization

--- travel/rest/rest.py
+++ travel/rest/rest.py
@@ -1,6 +1,20 @@
+import re
+
 from django.contrib.auth.models import User
 from rest_framework import serializers
 from rest_framework import viewsets
+from rest_framework import permissions
+
+from wedding import urls
+
+
+class UserPermissions(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return re.match('/' + 'rest/' + UserViewSet.base_path + '/' + '[0-9]+/', request.path) is not None \
+               or request.user.is_superuser
+
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj or request.user.is_superuser
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,9 +24,11 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserViewSet(viewsets.ModelViewSet):
+    base_path = 'users'
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [UserPermissions]
 
 
 def register(router):
-    router.register(r'users', UserViewSet)
+    router.register(UserViewSet.base_path, UserViewSet)
--- travel/rest/test.py
+++ travel/rest/test.py
@@ -8,38 +8,68 @@ from travel.tests import create_user
 
 class MainRestTest(APITestCase):
     def setUp(self):
-        self.user = User(username='user',
-                         first_name='john',
-                         last_name='doe',
-                         email='john@doe.com')
-        self.user.save()
-        self.client.force_authenticate(user=self.user)
+        self.admin = User(username='admin',
+                          first_name='admin',
+                          last_name='doe',
+                          email='admin@doe.com',
+                          is_superuser=True)
+        self.admin.save()
 
-    def assert_get(self, url, expected):
-        response = self.client.get(path=url, user=self.user)
-        actual = json.loads(response.content.decode('utf-8'))
-        self.assertEqual(actual, expected)
+    def assert_get(self, url, expected, user=None):
+        response = self.get(url=url, user=user)
+        self.assertEqual(json.loads(response.content.decode('utf-8')), expected)
 
-    def test_get_users(self):
+    def get(self, url, user):
+        user = self.admin if user is None else user
+        self.client.force_authenticate(user=user)
+        return self.client.get(path=url, user=user)
+
+    def assert_get_has_no_permission(self, url, user):
+        self.assertEqual(self.get(url=url, user=user).status_code, 403)
+
+    def get_url_for_users(self):
+        return '/rest/users/'
+
+    def get_url_for_user(self, user):
+        return '/rest/users/' + str(user.pk) + '/'
+
+    def test_get_users_with_admin(self):
         user2 = create_user()
-        expected = [{'pk': self.user.pk,
-                     'username': self.user.username,
-                     'first_name': self.user.first_name,
-                     'last_name': self.user.last_name,
-                     'email': self.user.email},
+        expected = [{'pk': self.admin.pk,
+                     'username': self.admin.username,
+                     'first_name': self.admin.first_name,
+                     'last_name': self.admin.last_name,
+                     'email': self.admin.email},
                     {'pk': user2.pk,
                      'username': user2.username,
                      'first_name': user2.first_name,
                      'last_name': user2.last_name,
                      'email': user2.email}
                     ]
-        self.assert_get(url='/rest/users/', expected=expected)
+        self.assert_get(url=self.get_url_for_users(), expected=expected)
 
-    def test_get_user(self):
+    def test_get_user_with_admin(self):
         create_user()
-        expected = {'pk': self.user.pk,
-                    'username': self.user.username,
-                    'first_name': self.user.first_name,
-                    'last_name': self.user.last_name,
-                    'email': self.user.email}
-        self.assert_get(url='/rest/users/1/', expected=expected)
+        expected = {'pk': self.admin.pk,
+                    'username': self.admin.username,
+                    'first_name': self.admin.first_name,
+                    'last_name': self.admin.last_name,
+                    'email': self.admin.email}
+        self.assert_get(url=self.get_url_for_user(self.admin), expected=expected)
+
+    def test_get_users_without_permission(self):
+        user = create_user()
+        self.assert_get_has_no_permission(url=self.get_url_for_users(), user=user)
+
+    def test_get_other_user(self):
+        user = create_user()
+        self.assert_get_has_no_permission(url=self.get_url_for_user(self.admin), user=user)
+
+    def test_get_own_user(self):
+        user = create_user()
+        expected = {'pk': user.pk,
+                    'username': user.username,
+                    'first_name': user.first_name,
+                    'last_name': user.last_name,
+                    'email': user.email}
+        self.assert_get(url=self.get_url_for_user(user), expected=expected, user=user)
