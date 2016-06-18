0fe1aa4927a94c00541097d2ddfcf940042413d4
keli <keli@keli.keli>
2016-06-18 23:22:31 +0200

    frontend: refactored travel.service.test

--- travel/static/travel/js/test/travel.service.test.js
+++ travel/static/travel/js/test/travel.service.test.js
@@ -14,71 +14,87 @@ describe('Given a Travel instance', function() {
     }));
     beforeEach(module('testUtils'));
 
+    var Travel,
+        TestUtils;
+    beforeEach(function() {
+        angular.mock.inject(function ($injector) {
+            Travel = $injector.get('Travel');
+            TestUtils = $injector.get('TestUtils');
+        });
+    });
+
     it('a passenger there can be added and only Travel.there will be set',
-        inject(function(Travel, TestUtils) {
-        Travel.addPassenger(TestUtils.createPassengerThere('1'));
-        expect(Travel.there.passenger.model.pk).toBe('1');
-        expect(Travel.there.driver.model).toBe(null);
-        expect(Travel.back.passenger.model).toBe(null);
-        expect(Travel.back.driver.model).toBe(null);
-    }));
+        function() {
+            Travel.addPassenger(TestUtils.createPassengerThere('1'));
+            expect(Travel.there.passenger.model.pk).toBe('1');
+            expect(Travel.there.driver.model).toBe(null);
+            expect(Travel.back.passenger.model).toBe(null);
+            expect(Travel.back.driver.model).toBe(null);
+        }
+    );
 
     it('when a passenger is added it will be emitted on the with one less free seat rootscope',
-        inject(function($rootScope, Travel, TestUtils) {
-        var passenger = TestUtils.createPassengerThere('1');
-        var emittedPassenger = TestUtils.createPassengerThere('1');
+        inject(function($rootScope) {
+            var passenger = TestUtils.createPassengerThere('1');
+            var emittedPassenger = TestUtils.createPassengerThere('1');
 
-        spyOn($rootScope, "$emit")
-        Travel.addPassenger(passenger);
-        expect($rootScope.$emit).toHaveBeenCalledWith("PASSENGER_ADDED", emittedPassenger);
-    }));
+            spyOn($rootScope, "$emit")
+            Travel.addPassenger(passenger);
+            expect($rootScope.$emit).toHaveBeenCalledWith("PASSENGER_ADDED", emittedPassenger);
+        })
+    );
 
     it('a passenger there and back can be added',
-        inject(function(Travel, TestUtils) {
-        Travel.addPassenger(TestUtils.createPassengerThere('1'));
-        Travel.addPassenger(TestUtils.createPassengerBack('2'));
-        expect(Travel.there.passenger.model.pk).toBe('1');
-        expect(Travel.there.driver.model).toBe(null);
-        expect(Travel.back.passenger.model.pk).toBe('2');
-        expect(Travel.back.driver.model).toBe(null);
-    }));
+        function() {
+            Travel.addPassenger(TestUtils.createPassengerThere('1'));
+            Travel.addPassenger(TestUtils.createPassengerBack('2'));
+            expect(Travel.there.passenger.model.pk).toBe('1');
+            expect(Travel.there.driver.model).toBe(null);
+            expect(Travel.back.passenger.model.pk).toBe('2');
+            expect(Travel.back.driver.model).toBe(null);
+        }
+    );
 
     it('a passenger there can be updated and only the Travel.there will be changed',
-        inject(function(Travel, TestUtils) {
-        Travel.addPassenger(TestUtils.createPassengerThere('1'));
-        Travel.addPassenger(TestUtils.createPassengerThere('2'));
-        expect(Travel.there.passenger.model.pk).toBe('2');
-        expect(Travel.there.driver.model).toBe(null);
-        expect(Travel.back.passenger.model).toBe(null);
-        expect(Travel.back.driver.model).toBe(null);
-    }));
+        function() {
+            Travel.addPassenger(TestUtils.createPassengerThere('1'));
+            Travel.addPassenger(TestUtils.createPassengerThere('2'));
+            expect(Travel.there.passenger.model.pk).toBe('2');
+            expect(Travel.there.driver.model).toBe(null);
+            expect(Travel.back.passenger.model).toBe(null);
+            expect(Travel.back.driver.model).toBe(null);
+        }
+    );
 
     it('when a passenger is deleted it will be emit on the rootscope',
-        inject(function($rootScope, Travel, TestUtils) {
-        var passenger = TestUtils.createPassengerThere('1');
-        var emittedPassenger = TestUtils.createPassengerThere('1');
-        Travel.addPassenger(passenger);
+        inject(function($rootScope) {
+            var passenger = TestUtils.createPassengerThere('1');
+            var emittedPassenger = TestUtils.createPassengerThere('1');
+            Travel.addPassenger(passenger);
 
-        spyOn($rootScope, "$emit")
-        Travel.there.passenger.remove(Travel.there.passenger);
-        expect($rootScope.$emit).toHaveBeenCalledWith("PASSENGER_DELETED", emittedPassenger);
-    }));
+            spyOn($rootScope, "$emit")
+            Travel.there.passenger.remove(Travel.there.passenger);
+            expect($rootScope.$emit).toHaveBeenCalledWith("PASSENGER_DELETED", emittedPassenger);
+        })
+    );
 
     it('a passenger can be deleted and then added',
-        inject(function(Travel, TestUtils) {
-        Travel.addPassenger(TestUtils.createPassengerThere('1'));
-        Travel.there.passenger.remove(Travel.there.passenger);
-        Travel.addPassenger(TestUtils.createPassengerThere('2'));
-        expect(Travel.there.passenger.model.pk).toBe('2');
-        expect(Travel.there.driver.model).toBe(null);
-        expect(Travel.back.passenger.model).toBe(null);
-        expect(Travel.back.driver.model).toBe(null);
-    }));
+        function() {
+            Travel.addPassenger(TestUtils.createPassengerThere('1'));
+            Travel.there.passenger.remove(Travel.there.passenger);
+            Travel.addPassenger(TestUtils.createPassengerThere('2'));
+            expect(Travel.there.passenger.model.pk).toBe('2');
+            expect(Travel.there.driver.model).toBe(null);
+            expect(Travel.back.passenger.model).toBe(null);
+            expect(Travel.back.driver.model).toBe(null);
+        }
+    );
 
     it('a isDriving returns false for passenger',
-        inject(function(Travel, TestUtils) {
-        Travel.addPassenger(TestUtils.createPassengerThere('1'));
-        expect(Travel.there.isDriving()).toBe(false);
-    }));
+        function() {
+            Travel.addPassenger(TestUtils.createPassengerThere('1'));
+            expect(Travel.there.isDriving()).toBe(false);
+        }
+    );
 
 });
