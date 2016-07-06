bdd43ea4e92d4cd0e60722a05c819b939f108567
keli <keli@keli.keli>
2016-07-06 12:10:35 +0200

    frontend: init theme and colors

--- travel/frontend/js/app.js
+++ travel/frontend/js/app.js
@@ -7,7 +7,7 @@
 
     angular.module('TravelServices', ['ngResource']);
 
-    function TravelAppConfig($resourceProvider, $httpProvider, $routeProvider) {
+    function TravelAppConfig($resourceProvider, $mdThemingProvider, $httpProvider, $routeProvider) {
         $resourceProvider.defaults.stripTrailingSlashes = false;
         $httpProvider.defaults.xsrfCookieName = 'csrftoken';
         $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
@@ -33,5 +33,13 @@
             redirectTo: '/rides',
             templateUrl: '/static/travel/templates/rides.html'
           });
+
+        $mdThemingProvider.theme('default')
+            .primaryPalette('indigo', {
+                'default': '400', // by default use shade 400 from the pink palette for primary intentions
+                'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
+                'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
+                'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+            });
     }
 }());
