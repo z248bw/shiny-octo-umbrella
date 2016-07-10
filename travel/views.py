df2e7d53c8c89182db6674caa7fab0e37314ae85
keli <keli@keli.keli>
2016-07-10 14:28:03 +0200

    frontend: wire in travel app

--- travel/views.py
+++ travel/views.py
@@ -2,12 +2,16 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 
+def index(request):
+    return render(request, 'travel/index.html')
+
+
 def register(request):
     return render(request, 'travel/register.html')
 
 
 @login_required
-def index(request):
+def main(request):
     return render(request, 'travel/main.html')
 
 
